40d32f05ec36c52de9959e71990fec60f792c4d2
emilys314 <emilys314@protonmail.com>
2023-05-12 00:43:48 +0000

    git ignore build stuff

--- hello_cargo/target/debug/deps/hello_cargo-ed094a605e0cc939.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust/hello_cargo/target/debug/deps/hello_cargo-ed094a605e0cc939.rmeta: src/main.rs
-
-/workspaces/rust/hello_cargo/target/debug/deps/hello_cargo-ed094a605e0cc939.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rust/hello_cargo/target/release/hello_cargo: /workspaces/rust/hello_cargo/src/main.rs
